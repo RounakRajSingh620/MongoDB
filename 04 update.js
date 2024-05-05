dc900881daa600f081914518e017a0b54dd1e77f
Rounak Raj Singh <97117309+RounakRajSingh620@users.noreply.github.com>
2024-05-05 12:05:44 +0530

    Update Operations

---
+++ 04 update.js
@@ -0,0 +1,20 @@
+
+db.inventory.find({ item: "paper" })
+
+db.inventory.updateOne(
+    { item: "paper" },
+    {
+        $set: { "size.uom": "cm", status: "P" },
+        $currentDate: { lastModified: true }
+    }
+)
+
+db.inventory.find({ "qty": { $lt: 50 } })
+
+db.inventory.updateMany(
+    { "qty": { $lt: 50 } },
+    {
+        $set: { "size.uom": "in", status: "P" },
+        $currentDate: { lastModified: true }
+    }
+)
